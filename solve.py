41a3d4c4eee1a788376dfb03856344423e849e60
Gxppe <giuseppe.queirolo@usm.cl>
2024-11-09 23:16:57 -0300

    En \resultados se genera la instancia junto a su tiempo de ejecucion y la cantidad de variables

--- solve.py
+++ solve.py
@@ -1,28 +1,42 @@
 import os
 import time
+import subprocess
 
-if os.sys.argv[1]:
-    timeout = os.sys.argv[1]
+
+
+
+
+# Obtener el valor del timeout desde los argumentos o usar un valor por defecto
+if len(os.sys.argv) > 1:
+    timeout = float(os.sys.argv[1])
 else:
     timeout = 1100
 
-
-# loop through files inside the directory "instancias"
+# Loop para recorrer archivos dentro del directorio "instancias"
 for filename in os.listdir("instancias"):
     if not filename.endswith(".lp"):
         continue
 
-    print(f"Solving instance {filename}...")
-    # Start timer here
-    time_0 = time.time()
-    os.system(f"lp_solve -presolve -presolveb -presolvec -B3 -Bc -Bo -s2 -gr 0.05 -f -timeout {timeout} -i -v3 -S2 instancias/{filename} > resultados/{filename.replace('.lp', '.txt')}")
-    time_f = time.time() - time_0
-    print(f"Instance solved in {time_f} seconds")
+    print(f"Resolviendo instancia {filename}...")
 
-    with open(f"resultados/{filename.replace('.lp', '.txt')}", "r") as file:
+    # Inicio del temporizador
+    time_0 = time.time()
+
+    # Ejecutar el comando lp_solve
+    os.system(f"lp_solve -timeout {timeout} instancias/{filename} > resultados/{filename.replace('.lp', '.txt')}")
+
+    # Calcular el tiempo de ejecución
+    time_f = time.time() - time_0
+    print(f"Instancia resuelta en {time_f:.2f} segundos")
+
+    # Leer el contenido del archivo de resultados
+    result_file_path = f"resultados/{filename.replace('.lp', '.txt')}"
+    with open(result_file_path, "r") as file:
         text = file.read()
 
-    with open(f"resultados/{filename.replace('.lp', '.txt')}", "w") as file:
-        print(time_f)
-        file.write(f"{float(time_f):.2f}\n")
-        file.write(text)
+    # Sobrescribir el archivo de resultados solo con el tiempo al principio
+    with open(result_file_path, "r") as file:
+        line_count = len(file.readlines())
+        with open(result_file_path, "w") as file:
+            file.write(f"{time_f:.2f} {line_count-4}")
+            file.write(text)
