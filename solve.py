b5605ee8bc5475c2ec8a47aef3b2c2ab8fada3bb
Markoker <repettojaramarco@gmail.com>
2024-11-09 22:09:09 -0300

    Solucion final definitiva ahora si (rapida)

--- solve.py
+++ solve.py
@@ -15,7 +15,14 @@ for filename in os.listdir("instancias"):
     print(f"Solving instance {filename}...")
     # Start timer here
     time_0 = time.time()
-    os.system(f"lp_solve -timeout {timeout} -t instancias/{filename} > resultados/{filename.replace('.lp', '.txt')}")
+    os.system(f"lp_solve -presolve -presolveb -presolvec -B3 -Bc -Bo -s2 -gr 0.05 -f -timeout {timeout} -i -v3 -S2 instancias/{filename} > resultados/{filename.replace('.lp', '.txt')}")
     time_f = time.time() - time_0
     print(f"Instance solved in {time_f} seconds")
 
+    with open(f"resultados/{filename.replace('.lp', '.txt')}", "r") as file:
+        text = file.read()
+
+    with open(f"resultados/{filename.replace('.lp', '.txt')}", "w") as file:
+        print(time_f)
+        file.write(f"{float(time_f):.2f}\n")
+        file.write(text)
